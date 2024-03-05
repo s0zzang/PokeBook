52ae66b833faf64a482bbe7b09d1af5e283acc0e
s0zzang <dlth8888@gmail.com>
2024-03-05 20:55:21 +0900

    refactor. loading html -> js, 로딩 후 remove로 개선

--- src/lib/utils/loading.js
+++ src/lib/utils/loading.js
@@ -1,23 +1,27 @@
 import { delay } from './index';
 import { gsap } from 'gsap';
 
-const $loading = document.querySelector('.loading');
-
-async function renderLoading() {
-  $loading.querySelector('img').src =
-    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${Math.floor(Math.random() * 100 + 1)}.gif`;
-  $loading.classList.add('is-active');
+const $app = document.querySelector('#app');
+export async function showLoading(time) {
+  const randomImgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${Math.floor(Math.random() * 251 + 1)}.gif`;
+  const template = `<div class="loading">
+      <img src="${randomImgSrc}" alt="랜덤 포켓몬" />
+      <p>
+        로딩중
+        <span>.</span>
+        <span>.</span>
+        <span>.</span>
+      </p>
+    </div>
+  `;
+  $app.insertAdjacentHTML('beforeend', template);
 
   const tl = gsap.timeline();
-  tl.to('.loading span', { display: 'inline-block', stagger: 0.3 }).to(
-    '.loading span',
-    { display: 'none', stagger: 0.3 },
-    '-=0.2'
-  );
-}
+  tl.to('.loading span', { display: 'inline-block', stagger: 0.3 })
+    .to('.loading span', { display: 'none', stagger: 0.3 }, '-=0.2')
+    .to('.loading span', { display: 'inline-block', stagger: 0.3 })
+    .to('.loading span', { display: 'none', stagger: 0.3 }, '-=0.2');
 
-export async function showLoading(time) {
-  renderLoading();
   await delay(time);
-  $loading.classList.remove('is-active');
+  document.querySelector('.loading').remove();
 }
